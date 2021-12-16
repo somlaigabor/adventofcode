090623c6d32a61d056483bfcbae8b5a2dd0df3e2
SomlaI Gábor <somlaigabor75@gmail.com>
2021-12-16 13:35:51 +0100

    Day 2 first half

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
 
-# # Advent of code day 1, first half
-# numbers = 0
-# k = None
-# f = open("input.txt", "r")
-# for line in f:
-#     if k:
-#         if int(k) < int(line):
-#             numbers += 1
-#     k = int(line)
-#
-# print(numbers)
+# Advent of code day 2, first half
 
-# Advent of code day 1, second half
-window = []
-actual_window_size = 3
+horizontal_position = 0
+depth = 0
 
 
-def sliding_window(array, new_item, window_size):
-    array.append(new_item)
-    if len(array) > window_size:
-        array.pop(0)
-    return array
+def slicing(data):
+    direction = data[:-3]
+    num = int(data[len(data)-2])
+    return {"num": num, "direction": direction}
 
 
-def a_sum(arr):
-    array_sum = 0
-    for i in arr:
-        array_sum = array_sum + i
-
-    return array_sum
-
-
-numbers = 0
-k = None
 f = open("input.txt", "r")
 for line in f:
-    window = sliding_window(window, int(line), actual_window_size)
-    if len(window) > 2:
-        if k:
-            if int(k) < a_sum(window):
-                numbers += 1
-        k = a_sum(window)
+    move = slicing(line)
+    if move['direction'] == 'forward':
+        horizontal_position += move['num']
+    
+    if move['direction'] == 'down':
+        depth += move['num']
 
-print(numbers)
+    if move['direction'] == 'up':
+        depth -= move['num']
+        
+    dd = 2
 
+print(horizontal_position*depth)
